23f126d5122526bf46c9b6e15fd10cd75a435067
tukka25 <aboodytukka@gmail.com>
2023-08-11 16:31:56 +0400

    hi

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+#include <unistd.h>
+#include <stdio.h>
+/**
+ *  main -Entry point
+ *
+ *  Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int	i;
+	char *str;
+
+	i = 0;
+	str = "abcdefghijklmnopqrstuvwxyz";
+	while (str[i])
+	{
+		putchar(str[i]);
+		i++;
+	}
+	putchar('\n');
+	return (0);
+}
