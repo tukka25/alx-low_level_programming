ea610752eb931fd0562a48b22de9ebfecdb9538c
tukka25 <aboodytukka@gmail.com>
2023-08-16 00:32:35 +0400

    2

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -29,8 +29,8 @@ void print_alphabet_x10(void)
  *  Return: Always 0 (Success)
  */
 
-int main(void)
-{
-	print_alphabet_x10();
-	return (0);
-}
+// int main(void)
+// {
+// 	print_alphabet_x10();
+// 	return (0);
+// }
