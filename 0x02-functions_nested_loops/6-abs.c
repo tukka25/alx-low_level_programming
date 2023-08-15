71c1f1195d4a8a83c02c961e7447cdabff60fa2a
tukka25 <aboodytukka@gmail.com>
2023-08-16 00:59:35 +0400

    20

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ *  _abs - calculate the absolute value
+ *	@n: int
+ *	Return: return the absolute value
+ */
+
+int _abs(int n)
+{
+	if (n >= 0)
+		return (n);
+	else
+		return (-1 * n);
+}
