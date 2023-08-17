0741f76bc950a9be066c08590a0ef77be54c653c
tukka25 <aboodytukka@gmail.com>
2023-08-17 14:29:10 +0400

    isdigit

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -10,19 +10,19 @@ void more_numbers(void)
 {
 	int j = 0;
 	int	i = 0;
+	int	k = 0;
 
 	while (j < 10)
 	{
 		i = 0;
 		while (i <= 14)
 		{
+			k = i;
+			if (i <= 9)
+				k = i * 10;
+			_putchar((k / 10) + 48);
 			if (i >= 10)
-			{
-				_putchar((i / 10) + 48);
 				_putchar((i % 10) + 48);
-			}
-			else
-				_putchar(i + 48);
 			i++;
 		}
 		_putchar('\n');
