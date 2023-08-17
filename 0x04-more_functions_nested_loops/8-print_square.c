986a9b838f1153aaa5b82bfc9b2ae2b486330499
tukka25 <aboodytukka@gmail.com>
2023-08-17 20:22:57 +0400

    isdigit

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -1,8 +1,28 @@
 #include "main.h"
+#include <stdlib.h>
 
 /**
- * print_diagonal - display '\' n times
+ * print_square - display sqaure of '#'
  *
- * @n: number of '\'
+ * @size: number of '#'
  * Return: void
  */
+void print_square(int size)
+{
+	int	i = 0;
+	int	j = 0;
+
+	if (size <= 0)
+		_putchar('\n');
+	while (i < size)
+	{
+		j = 0;
+		while (j < size)
+		{
+			_putchar('#');
+			j++;
+		}
+		_putchar('\n');
+		i++;
+	}
+}
