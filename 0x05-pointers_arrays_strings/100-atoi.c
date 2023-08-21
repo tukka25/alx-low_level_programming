35fe3dad3b9a4b81aac57212583d9828265f4c30
tukka25 <aboodytukka@gmail.com>
2023-08-21 19:06:47 +0400

    inshalla

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,34 @@
+#include "main.h"
+# include <stdlib.h>
+
+/**
+ * _atoi - convert ascii to int
+ *
+ * @s: a char * to convert
+ *
+ * Return: int
+ */
+
+int _atoi(char *s)
+{
+	int i = 0;
+	int	sign = 1;
+	int result = 0;
+
+	while ((s[i] >= 9 && s[i] <= 13) || (s[i] == ' '))
+		i++;
+	if (s[i] == '-' || s[i] == '+')
+	{
+		if (s[i] == '-')
+			sign *= -1;
+		i++;
+	}
+	while (s[i] == '0')
+		i++;
+	while (s[i] >= '0' && s[i] <= '9')
+	{
+		result = result * 10 + s[i] - '0';
+		i++;
+	}
+	return (result * sign);
+}
