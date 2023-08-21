ed604a4c6caddb2d188cdc7f0d0d48bd72f4aa50
tukka25 <aboodytukka@gmail.com>
2023-08-21 18:49:51 +0400

    inshalla

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ *  print_array - copies from src to dst
+ *
+ * @dest: a char * to copy to
+ * @src: a char * to copy from
+ *
+ * Return: char * dest
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+	int	i = 0;
+
+	while (src[i++])
+		dest[i] = src[i];
+	dest[i] = '\0';
+	return (dest);
+}
