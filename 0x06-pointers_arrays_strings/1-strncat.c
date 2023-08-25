7f516b551f7ffc4e90882ba43cf1c3cdb477d126
tukka25 <aboodytukka@gmail.com>
2023-08-25 05:44:43 +0400

    6

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,29 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ *  _strncat - concatenate two strings to number of n
+ *
+ * @src: a src string
+ * @dest: a dest string
+ * @n: a int number of char
+ *
+ * Return: concatenated string
+ */
+
+char *_strncat(char *dest, char *src, int n)
+{
+	int	i = 0;
+	int	j = 0;
+
+	while (dest[i])
+		i++;
+	while (src[j] && i < n)
+	{
+		dest[i] = src[j];
+		i++;
+		j++;
+	}
+	dest[i] = '\0';
+	return (dest);
+}
