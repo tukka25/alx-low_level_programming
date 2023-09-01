3aa3500445aaa09a4e0239c5b1387f423aef176d
tukka25 <aboodytukka@gmail.com>
2023-09-02 02:03:10 +0400

    f

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ *	main -Entry point
+ *	@ac:number of args
+ *	@av:args vector
+ *	Return: Always 0 (Success)
+ */
+
+int main(int ac, char *av[])
+{
+	(void)ac;
+	printf("%s\n", av[0]);
+	return (0);
+}
