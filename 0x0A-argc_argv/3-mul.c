a23aad4987105915ae67ecb45a75717fa3c757ce
tukka25 <aboodytukka@gmail.com>
2023-09-02 02:17:23 +0400

    fkkk

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+/**
+ *	main - prints all arguments it receives.
+ *	@ac:number of args
+ *	@av:args vector
+ *	Return: Always 0 (Success)
+ */
+
+int main(int ac, char **av)
+{
+	if (ac != 3)
+	{
+		write(1, "Error\n", 6);
+		return (1);
+	}
+	printf("%d\n", atoi(av[1]) * atoi(av[2]));
+}
