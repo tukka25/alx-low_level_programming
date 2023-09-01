4b2b44998ffd7799eb4f53dceab7a255fdc29f87
tukka25 <aboodytukka@gmail.com>
2023-09-02 02:27:25 +0400

    g

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <ctype.h>
+/**
+ *	main - prints all arguments it receives.
+ *	@ac:number of args
+ *	@av:args vector
+ *	Return: Always 0 (Success)
+ */
+
+int main(int ac, char **av)
+{
+	int	i = 1;
+	int	j = 0;
+
+	if (ac == 1)
+	{
+		write(1, "Error\n", 6);
+		return (1);
+	}
+	while (av[i])
+	{
+		j = 0;
+		while (av[i][j])
+		{
+			if (av[i][j] && !isdigit(av[i][j]))
+			{
+				write(1, "Error\n", 6);
+				return (1);
+			}
+			j++;
+		}
+		i++;
+	}
+	i = 1;
+	j = 0;
+	while (av[i])
+	{
+		j += atoi(av[i]);
+		i++;
+	}
+	printf("%d\n", j);
+	return (0);
+}
