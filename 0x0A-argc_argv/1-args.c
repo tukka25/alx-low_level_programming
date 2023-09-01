0b89fb396e1e13e7175df74a2640eabf6fe40373
tukka25 <aboodytukka@gmail.com>
2023-09-02 02:04:25 +0400

    f

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ *	main -Entry point
+ *	@ac:number of args
+ *	@av:args vector
+ *	Return: Always 0 (Success)
+ */
+
+int main(int ac, char *av[])
+{
+	(void)av;
+	printf("%d\n", ac);
+	return (0);
+}
