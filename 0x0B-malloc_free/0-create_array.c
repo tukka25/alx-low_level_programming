809395b491e1df49f9e153684b31434a10275fef
tukka25 <aboodytukka@gmail.com>
2023-09-04 11:46:25 +0400

    malloc

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -16,7 +16,7 @@ char *create_array(unsigned int size, char c)
 
 	if (size < 0)
 		return (NULL);
-	str = malloc (size + 1);
+	str = malloc(size + 1);
 	if (!str)
 		return (NULL);
 	while (i < (int)size)
