e7a1cd057347371ead67dbddf5aa781ac48e3f79
tukka25 <aboodytukka@gmail.com>
2023-09-04 12:08:55 +0400

    malloc

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ *	_strdup - creates an array
+ *	@str: a string
+ *	Return: char *
+ */
+
+char *_strdup(char *str)
+{
+	int i = 0;
+	int j = 0;
+	char *s = NULL;
+
+	if (!str)
+		return (NULL);
+	while (str[i])
+		i++;
+	s = malloc(i + 1);
+	while (str[j])
+	{
+		s[j] = str[j];
+		j++;
+	}
+	s[j] = '\0';
+	return (s);
+}
