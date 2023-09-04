c97ef05a3dcfbab0b43692658812b70f3a963ee1
tukka25 <aboodytukka@gmail.com>
2023-09-04 17:56:51 +0400

    malloc

---
+++ 0x0B-malloc_free/101-strtow.c
@@ -0,0 +1,73 @@
+#include "main.h"
+
+/**
+ *	len - return len of 2d array
+ *	@str: an array
+ *	Return: int
+ */
+
+static int	len(char *str)
+{
+	int i = 0;
+	int length = 0;
+
+	if (!str)
+		return (0);
+	while (str[i])
+	{
+		if (i == 0 && str[i] != ' ')
+			length++;
+		else if (str[i] != ' ' && str[i - 1] == ' ')
+			length++;
+		i++;
+	}
+	return (length);
+}
+
+/**
+ *	strtow - split
+ *	@str: an array
+ *	Return: char **
+ */
+char **strtow(char *str)
+{
+	int i = 0;
+	int j = 0;
+	int	len1 = 0;
+	char	**s = NULL;
+
+	if (!str || !*str)
+		return (NULL);
+	len1 = len(str);
+	s = malloc((len1 + 1) * sizeof(char *));
+	if (!s)
+		return (NULL);
+	while (*str)
+	{
+		j = 0;
+		while (str && *str == ' ')
+			str++;
+		if (*str == ' ')
+			str++;
+		while (str[j] && str[j] != ' ')
+			j++;
+		s[i] = malloc(j + 1);
+		if (!s[i])
+		{
+			free(s[i]);
+			free(s);
+			return (NULL);
+		}
+		j = 0;
+		while (*str && *str != ' ')
+		{
+			s[i][j] = *str;
+			j++;
+			str++;
+		}
+		s[i][j] = '\0';
+		i++;
+	}
+	s[i] = NULL;
+	return (s);
+}
