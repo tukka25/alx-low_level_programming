7cb31a9a1c894f490b3d597bcf9e6460f8194e9d
tukka25 <aboodytukka@gmail.com>
2023-09-04 16:26:49 +0400

    malloc

---
+++ 0x0B-malloc_free/100-argstostr.c
@@ -0,0 +1,48 @@
+#include "main.h"
+
+/**
+ *	argstostr - args to str
+ *	@ac: a array vector len
+ *	@av: an array vector
+ *	Return: char *
+ */
+
+char *argstostr(int ac, char **av)
+{
+	int	length = 0;
+	int	i = 0;
+	int	j = 0;
+	char	*str = NULL;
+
+	if (!ac || !av)
+		return (NULL);
+	while (av[i])
+	{
+		j = 0;
+		while (av[i][j])
+		{
+			length++;
+			j++;
+		}
+		i++;
+	}
+	str = malloc(length + ac - 1);
+	i = 0;
+	j = 0;
+	length = 0;
+	while (av[i])
+	{
+		j = 0;
+		while (av[i][j])
+		{
+			str[length] = av[i][j];
+			j++;
+			length++;
+		}
+		str[length] = '\n';
+		length++;
+		i++;
+	}
+	return (str);
+
+}
