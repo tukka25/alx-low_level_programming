9d3f74856b165c8d55beb9082727d2af2471ae2a
tukka25 <aboodytukka@gmail.com>
2023-09-04 14:35:53 +0400

    malloc

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -16,9 +16,21 @@ int **alloc_grid(int width, int height)
 	if (width <= 0 && height <= 0)
 		return (NULL);
 	arr = malloc((height + 1) * sizeof(int *));
+	if (!arr)
+		return (NULL);
 	while (i < height)
 	{
 		arr[i] = malloc((width + 1) * sizeof(int));
+		if (!arr[i])
+		{
+			while (i > 0)
+			{
+				free(arr[i]);
+				i--;
+			}
+			free(arr);
+			return (NULL);
+		}
 		j = 0;
 		while (j < width)
 		{
